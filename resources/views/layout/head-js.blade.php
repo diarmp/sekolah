77fcc63d4718b19dc303d754d583a991196ead70
jamalul-insan <101683243+jamalul-insan@users.noreply.github.com>
2023-03-21 14:04:14 +0700

    #install datatable and sweet allert (#6)
    * add: install fortify
    
    * add:login process and template
    
    * add: dashboard and logout fiture
    
    * modif: disable registration
    
    * modif:untuk title bar, formatnya nama halaman | APP_NAME
    
    * modif: change image login and title login
    
    * add: dummy school
    
    * add:datatables
    
    * add: set config datatable
    
    * add: composer lock update
    
    * add: componet datatable
    
    * add: install datatables and sweeat alert

--- resources/views/layout/head-js.blade.php
+++ resources/views/layout/head-js.blade.php
@@ -1,3 +1,5 @@
+@include('sweetalert::alert')
+
 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
