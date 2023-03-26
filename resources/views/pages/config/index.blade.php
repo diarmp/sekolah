a2a46318add3e44cdfe2ab4fa3c59a13bb97997a
Diar <swinub15@gmail.com>
2023-03-26 08:22:10 +0700

    new design

--- resources/views/pages/config/index.blade.php
+++ resources/views/pages/config/index.blade.php
@@ -8,13 +8,17 @@
 
         {{-- start table academy years --}}
         <div class="col-lg-10">
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">{{$title}}</h1>
+            <a href="{{ route('master-configs.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">Tambah Config</a>
+        </div>
             <div class="card">
-                <div class="card-header d-flex">
+                <!-- <div class="card-header d-flex">
                     <h6 class="mr-auto font-weight-bold text-primary">{{ $title }}</h6>
-                    <a href="{{ route('master-configs.create') }}" class="btn btn-primary btn-sm">ADD</a>
-                </div>
+                    <a href="{{ route('master-configs.create') }}" class="btn btn-primary btn-sm">Tambah Configurasi</a>
+                </div> -->
                 <div class="card-body">
-                    <x-datatable :id="'master-configs'" :headers="['Config Code', 'Name', 'Action']" :url="route('datatable.master-configs')" />
+                    <x-datatable :id="'master-configs'" :headers="['Config Code', 'Name', ' ']" :url="route('datatable.master-configs')" />
                 </div>
             </div>
         </div>
