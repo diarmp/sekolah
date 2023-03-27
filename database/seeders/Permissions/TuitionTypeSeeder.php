6b1a578629592b52a6213f07d3188b6bdf7fbdd0
AaEzha <aaezha@gmail.com>
2023-03-27 11:06:13 +0700

    chore: tuition type permissions 🔒

---
+++ database/seeders/Permissions/TuitionTypeSeeder.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Seeders\Permissions;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class TuitionTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $super_admin = Role::whereName(User::ROLE_SUPER_ADMIN)->first();
+        $ops_admin = Role::whereName(User::ROLE_OPS_ADMIN)->first();
+        $admin_sekolah = Role::whereName(User::ROLE_ADMIN_SEKOLAH)->first();
+        $admin_yayasan = Role::whereName(User::ROLE_ADMIN_YAYASAN)->first();
+        $tata_usaha = Role::whereName(User::ROLE_TATA_USAHA)->first();
+        $bendahara = Role::whereName(User::ROLE_BENDAHARA)->first();
+        $kepala_sekolah = Role::whereName(User::ROLE_KEPALA_SEKOLAH)->first();
+
+        $roles = [
+            'index' => 'tuition-type.index',
+            'create' => 'tuition-type.create',
+            'store' => 'tuition-type.store',
+            'edit' => 'tuition-type.edit',
+            'update' => 'tuition-type.update',
+            'destroy' => 'tuition-type.destroy'
+        ];
+
+        // index
+        $permission = Permission::firstOrCreate([
+            'name' => $roles['index'],
+            'guard_name' => 'web'
+        ]);
+        $permission->syncRoles([$super_admin, $ops_admin, $admin_sekolah, $admin_yayasan, $kepala_sekolah, $tata_usaha, $bendahara]);
+
+        // create school
+        $permission = Permission::firstOrCreate([
+            'name' => $roles['create'],
+            'guard_name' => 'web'
+        ]);
+        $permission->syncRoles([$super_admin, $ops_admin, $bendahara]);
+        $permission = Permission::firstOrCreate([
+            'name' => $roles['store'],
+            'guard_name' => 'web'
+        ]);
+        $permission->syncRoles([$super_admin, $ops_admin, $bendahara]);
+
+        // update school
+        $permission = Permission::firstOrCreate([
+            'name' => $roles['edit'],
+            'guard_name' => 'web'
+        ]);
+        $permission->syncRoles([$super_admin, $ops_admin, $bendahara]);
+        $permission = Permission::firstOrCreate([
+            'name' => $roles['update'],
+            'guard_name' => 'web'
+        ]);
+        $permission->syncRoles([$super_admin, $ops_admin, $bendahara]);
+
+        // destroy school
+        $permission = Permission::firstOrCreate([
+            'name' => $roles['destroy'],
+            'guard_name' => 'web'
+        ]);
+        $permission->syncRoles([$super_admin, $ops_admin, $bendahara]);
+    }
+}
