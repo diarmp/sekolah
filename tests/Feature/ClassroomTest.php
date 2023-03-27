10aa5bce8ef6098f42847ceee45b949baee5ea27
reynold <reynold@marketport.io>
2023-03-27 09:42:00 +0700

    added pest unit test classroom

--- tests/Feature/ClassroomTest.php
+++ tests/Feature/ClassroomTest.php
@@ -2,9 +2,9 @@
 
 use App\Models\Grade;
 use App\Models\School;
+use App\Models\Classroom;
 use App\Models\AcademicYear;
-use Illuminate\Foundation\Testing\WithFaker;
-
+use Faker\Factory as FakerFactory;
 
 it('can create Classroom', function () {
     
@@ -28,25 +28,51 @@ it('can create Classroom', function () {
 });
 
 it('can update Classroom', function () {
+    $faker = FakerFactory::create();
     
     $school = School::factory()->create();
     $academicYear = AcademicYear::factory()->create(['school_id' => $school->id]);
     $grade = Grade::factory()->create(['school_id' => $school->id]);
-        
-    $newClassroom = $this->faker->randomElements(['1B', '2C', '3A', '4E', '5D']);
-
-    $classroom = $school->classrooms()->update([
+    
+    $classroom = $school->classrooms()->create([
         'school_id' => $school->id,
         'academic_year_id' => $academicYear->id,
         'grade_id' => $grade->id,
+    ]);
+    
+    $newClassroom = $faker->randomElement(['1A', '2E', '3B', '4C', '5D']);
+
+    $classroom = $school->classrooms()->update([
         'name' => $newClassroom
     ]);
 
     $this->assertDatabaseHas('classrooms', [
-        'id' => $classroom->id,
         'school_id' => $school->id,
         'academic_year_id' => $academicYear->id,
         'grade_id' => $grade->id,
         'name' => $newClassroom
     ]);
 });
+
+it('can delete Classroom', function () {
+    $faker = FakerFactory::create();
+    
+    $school = School::factory()->create();
+    $academicYear = AcademicYear::factory()->create(['school_id' => $school->id]);
+    $grade = Grade::factory()->create(['school_id' => $school->id]);
+
+
+    $classroom = $school->classrooms()->create([
+        'school_id' => $school->id,
+        'academic_year_id' => $academicYear->id,
+        'grade_id' => $grade->id,
+        'name' => $faker->randomElement(['1A', '2E', '3B', '4C', '5D'])
+    ]);
+
+    $classroom->delete();
+
+    $this->assertSoftDeleted('classrooms', [
+        'id' => $classroom->id
+    ]);
+});
+
