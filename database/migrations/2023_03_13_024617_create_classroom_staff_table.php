fe7501f1fb69adba3c52acc32e056055fa6a4ed7
Aa Ezha <100393465+backend-sempoa@users.noreply.github.com>
2023-03-27 10:13:00 +0700

    Update 2023_03_13_024617_create_classroom_staff_table.php

--- database/migrations/2023_03_13_024617_create_classroom_staff_table.php
+++ database/migrations/2023_03_13_024617_create_classroom_staff_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Classroom;
+use App\Models\School;
 use App\Models\Staff;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('classroom_staff', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(School::class)->nullable()->constrained()->nullOnDelete();
             $table->foreignIdFor(Staff::class)->nullable()->constrained()->nullOnDelete();
             $table->timestamps();
             $table->softDeletes();
