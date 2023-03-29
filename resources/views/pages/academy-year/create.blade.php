9a8af809b0521f8be230a032ff7fb179420dd491
jamalul-insan <101683243+jamalul-insan@users.noreply.github.com>
2023-03-29 12:00:11 +0700

    feat: Academy Years dan Tuition Type (#51)
    * add: route assign class
    
    * ♻️ feat: academy years
    
    * 🐛 feat: tuition type
    
    * 🎉  begin: assign classroom

--- resources/views/pages/academy-year/create.blade.php
+++ resources/views/pages/academy-year/create.blade.php
@@ -9,35 +9,25 @@
 
         {{-- start table academy years --}}
         <div class="col-lg-6">
+            @error('school_id')
+                <div class="alert alert-danger" role="alert">
+                    {{ $message }}
+                </div>
+            @enderror
+
             <div class="card">
                 <div class="card-header d-flex">
                     <h6 class="mr-auto font-weight-bold text-primary">{{ $title }}</h6>
-
                 </div>
                 <div class="card-body">
                     <form action="{{ route('academy-year.store') }}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="school-select">Sekolah</label>
-                            <select class="form-control @error('school_id') is-invalid @enderror" name="school_id"
-                                id="school-select">
-                                <option value="">-</option>
-                                @foreach ($schools as $school)
-                                    <option value="{{ $school->id }}">
-                                        {{ $school->name }}
-                                    </option>
-                                @endforeach
-                            </select>
-                            @error('school_id')
-                                <div class="invalid-feedback">
-                                    {{ $message }}
-                                </div>
-                            @enderror
-                        </div>
-                        <div class="form-group">
+                            <input type="hidden" name="school_id" value="{{ session('school_id') }}">
                             <label for="year-academy-input">Tahun Akademik</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
-                                id="year-academy-input" placeholder="2019 - 2020">
+                            <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                value="{{ old('name') }}" name="name" id="year-academy-input"
+                                placeholder="2019 - 2020">
                             @error('name')
                                 <div class="invalid-feedback">
                                     {{ $message }}
