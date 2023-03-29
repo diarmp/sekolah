5c284c548b58a4f6d7b1a75f2b8273c653e40e05
Aa Ezha <100393465+backend-sempoa@users.noreply.github.com>
2023-03-29 12:02:36 +0700

    feat(general): Sidebar dinamis (#46)
    * wip 🔥
    
    * install package LaravelEasyNav 🔥
    
    * Redesign school page 🔥
    
    * sidebar dinamis 🔥
    
    ---------
    
    Co-authored-by: AaEzha <aaezha@gmail.com>

--- resources/views/layout/topbar-search.blade.php
+++ resources/views/layout/topbar-search.blade.php
@@ -1,40 +1,43 @@
 @hasanyrole('super admin|ops admin')
-  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-    <select id="school_selector" name="school_selector"
-      class="form-control select2 @error('school_selector') is-invalid @enderror" required>
-      <option value="">Pilih Sekolah ...</option>
-      @foreach ($school_selectors as $school_selector)
-        <option value="{{ $school_selector->getKey() }}" @selected($school_selector->getKey() == session('school_id'))>{{ $school_selector->name }}</option>
-      @endforeach
-    </select>
-  </form>
-  @push('js')
-    <script>
-      $(function() {
-        $('#school_selector').change(function() {
-          const token = $('meta[name="csrf-token"]').attr('content');
-          const data = {
-            _token: token,
-            school_selector: $(this).val()
-          };
-          fetch(route('school_selector'), {
-              method: "POST", // or 'PUT'
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(data),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-              console.log("Success:", data);
-              window.location.reload()
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-              window.location.reload()
-            });
+  @if (!navIsResource('schools') and !navIsResource('master-configs'))
+    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+      <select id="school_selector" name="school_selector"
+        class="form-control select2 @error('school_selector') is-invalid @enderror" required>
+        <option value="">Pilih Sekolah ...</option>
+        @foreach ($school_selectors as $school_selector)
+          <option value="{{ $school_selector->getKey() }}" @selected($school_selector->getKey() == session('school_id'))>{{ $school_selector->name }}
+          </option>
+        @endforeach
+      </select>
+    </form>
+    @push('js')
+      <script>
+        $(function() {
+          $('#school_selector').change(function() {
+            const token = $('meta[name="csrf-token"]').attr('content');
+            const data = {
+              _token: token,
+              school_selector: $(this).val()
+            };
+            fetch(route('school_selector'), {
+                method: "POST", // or 'PUT'
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              })
+              .then((response) => response.json())
+              .then((data) => {
+                console.log("Success:", data);
+                window.location.reload()
+              })
+              .catch((error) => {
+                console.error("Error:", error);
+                window.location.reload()
+              });
+          });
         });
-      });
-    </script>
-  @endpush
+      </script>
+    @endpush
+  @endif
 @endrole
